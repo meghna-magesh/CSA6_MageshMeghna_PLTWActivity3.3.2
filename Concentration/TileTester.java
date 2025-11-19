0fbe87ca98c2189ee836bfb11ec7ed7713e2b7b8
Meghna Magesh <mages8078@mydusd.org>
2025-11-19 15:40:51 -0800

    first commit

---
+++ Concentration/TileTester.java
@@ -0,0 +1,35 @@
+/*
+ * Activity 3.3.1
+ */
+public class TileTester
+{
+  private static String[] tileValues =
+  { "lion", "lion",
+    "penguin", "penguin",
+    "dolphin", "dolphin",
+    "fox", "fox",
+    "monkey", "monkey",
+    "turtle", "turtle" }; 
+
+  public static void main(String[] args)
+  {
+    
+    /* your code here */
+    Tile[][] gameboard = new Tile[3][4];
+
+    for (int row = 0; row < gameboard.length; row++) {
+        for (int col = 0; col < gameboard[row].length; col++) {
+            int index = row * gameboard[row].length + col;
+            gameboard[row][col] = new Tile(tileValues[index]);
+        }
+    }
+  
+    for (int row = 0; row < gameboard.length; row++) {
+        for (int col = 0; col < gameboard[row].length; col++) {
+            int index = row * gameboard[row].length + col;
+        System.out.println(gameboard[row][col]);
+    }
+}
+    
+  }
+}
